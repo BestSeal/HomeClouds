c5d36afaa6269e372e3196f8662e48cb6ccb1a4e
BestSeal <thatlazyseal@gmail.com>
2020-12-09 13:36:35 +0300

    Basic filter

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/AuthFilter.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/AuthFilter.java
@@ -1,88 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.mavenproject1;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import static java.util.Objects.nonNull;
+import javax.servlet.http.*;
+
 
 /**
  *
- * @author docuc
+ * @author Ilya G
  */
-@WebServlet(name = "AuthFilter", urlPatterns = {"/AuthFilter"})
-public class AuthFilter extends HttpServlet {
+@WebFilter(urlPatterns = "/*")
+public class AuthFilter implements Filter {
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException{};
+    
+    @Override
+    public void destroy() {
+    }
+    
+    @Override
+    public void doFilter(final ServletRequest request,
+                         final ServletResponse response,
+                         final FilterChain filterChain)
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AuthFilter</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AuthFilter at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            throws IOException, ServletException {
+      
+    String login = request.getParameter("login");
+    String pass = request.getParameter("password"); //we shoud add encryption or hashing as pass is stored in the cookie
+    HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletResponse res = (HttpServletResponse) response;
+    HttpSession session =  req.getSession();
+           
+    if(nonNull(session) && nonNull(session.getAttribute("login")) && nonNull(session.getAttribute("password"))){
+        int status = (int) session.getAttribute("role");
+        redirect(req, res, status);
+    }
+
+        redirect(req, res, 1);
+    }
+       
+    private void redirect(HttpServletRequest req, HttpServletResponse res, int status) throws ServletException, IOException
+    {
+        if (status == 1)
+        {
+            req.getRequestDispatcher("/WEB-INF/view/login_page.jsp").forward(req, res);
+        }
+        else 
+        {
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
